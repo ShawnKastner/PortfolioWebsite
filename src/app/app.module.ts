44699b63f21d6bf471cb1ae46837570c62d336c1
Shawn Kastner <Shawnkastner@hotmail.de>
2023-03-28 12:09:00 +0200

    added above the fold section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboveTheFoldComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
