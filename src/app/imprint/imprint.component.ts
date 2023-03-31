0b469ab9c8a16cc57f94eb0811927c49b26ae474
Shawn Kastner <Shawnkastner@hotmail.de>
2023-03-31 12:03:03 +0200

    footer responsive and function to scroll to top when clicked on imprint

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class ImprintComponent {
 
+  ngAfterViewInit() {
+    // Hack: Scrolls to top of Page after page view initialized
+    let top = document.getElementById('top');
+    if (top !== null) {
+      top.scrollIntoView();
+      top = null;
+    }
+  }
+
 }
